8e10c7e89fcfdfe94a01a43a129ba86927fb12bd
anygizer <mcangel@mcangel-laptop>
2011-09-08 00:22:24 +0300

    Child of both parents rule was added. Sibling rule was recoded - now it\'s based on the ternary child rule and has relationship trough one of the parents. Niece rule was fixed.

--- genealogy/engine.pl
+++ genealogy/engine.pl
@@ -3,6 +3,7 @@
 	father/2,
 	mother/2,
 	child/2,
+	child/3,
 	son/2,
 	daughter/2,
 	grandparent/2,
@@ -24,6 +25,7 @@ father(X,Y):-parent(X,Y),male(X).
 mother(X,Y):-parent(X,Y),female(X).
 
 child(X,Y):-parent(Y,X).
+child(C,X,Y) :- father(X,C),mother(Y,C),\==(X,Y).
 son(X,Y):-child(X,Y),male(X).
 daughter(X,Y):-child(X,Y),female(X).
 
@@ -33,7 +35,11 @@ grandchild(X,Z):-grandparent(Z,X).
 predecessor(X,Z):-parent(X,Z);parent(X,Y),predecessor(Y,Z).
 descendant(X,Z):-predecessor(Z,X).
 
-sibling(X,Z):-father(Y,X),father(Y,Z),mother(W,X),mother(W,Z),\==(X,Z).
+sibling(X,Y) :- (
+                  (child(X,M,F),child(Y,M,F))
+                  ;(child(X,P),child(Y,P),not((child(X,M,F),child(Y,M,F))))
+                )
+                ,\==(X,Y).
 brother(X,Y):-sibling(X,Y),male(X).
 sister(X,Y):-sibling(X,Y),female(X).
 
@@ -41,7 +47,7 @@ uncle(X,Y):-parent(Z,Y),brother(X,Z).
 aunt(X,Y):-parent(Z,Y),sister(X,Z).
 
 nephew(X,Y):-sibling(Z,Y),son(X,Z).
-niece(X,Y):-sibling(Z,Y),sister(X,Z).
+niece(X,Y):-sibling(Z,Y),daughter(X,Z).
 
 cousin2(X,Y):-parent(Z,X),sibling(W,Z),child(Y,W).
 cousin(X,Y):-sibling(X,Y);parent(Z,X),cousin(W,Z),child(Y,W).
