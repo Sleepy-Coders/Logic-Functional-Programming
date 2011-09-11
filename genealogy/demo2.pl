ed7c12a48ada5feac3b68509919a3b776c5fa30c
anygizer <mcangel@mcangel-laptop>
2011-09-12 00:24:07 +0300

    Somehow fixed extra module - now sister/2, brother/2, uncle/2 and aunt/2 works correctly.
    TODO: findout how to deprecate usage of excepted rules from module.

--- genealogy/demo2.pl
+++ genealogy/demo2.pl
@@ -1,10 +1,11 @@
-:- use_module(engine/main,except([sibling/2])).
+:- use_module(engine/main,except([sibling/2,brother/2,sister/2,uncle/2,aunt/2])).
 :- use_module(engine/extra).
 
 male('syn').
 male('tato').
 male('dido').
 female('sestra').
+female('o.sestra').
 female('mama').
 female('baba').
 female('tsiotsia').
@@ -14,6 +15,7 @@ parent('tato','syn').
 parent('mama','syn').
 parent('mama','sestra').
 parent('tato','sestra').
+parent('mama','o.sestra').
 parent('dido','tato').
 parent('baba','tato').
 parent('dido','tsiotsia').
--- genealogy/engine/extra.pl
+++ genealogy/engine/extra.pl
@@ -1,12 +1,21 @@
 :- module(extra_engine,
 	[
 	sibling/2,
-	child/3
+	child/3,
+	brother/2,
+	sister/2,
+	uncle/2,
+	aunt/2
 	]).
 
 child(C,X,Y) :- father(X,C),mother(Y,C),\==(X,Y).
-sibling(X,Y) :-  
-                (
+sibling(X,Y) :- ( 
                   (child(X,M,F),child(Y,M,F))
                   ;(child(X,P),child(Y,P),not((child(X,M,F),child(Y,M,F))))
                 ),(X\=Y).
+
+brother(X,Y):-sibling(X,Y),male(X).
+sister(X,Y):-sibling(X,Y),female(X).
+
+uncle(X,Y):-parent(Z,Y),brother(X,Z).
+aunt(X,Y):-parent(Z,Y),sister(X,Z).
