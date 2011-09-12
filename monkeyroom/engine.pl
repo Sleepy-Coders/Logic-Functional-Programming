c230078cecbfaf88f9d6057deb3d7832a8651bfa
anygizer <mcangel@unikernel.net>
2011-09-13 02:10:46 +0300

    Actions and positions specified.
    Now monkey can choose from known positions and actions - wiser monkey ^_^.

--- monkeyroom/engine.pl
+++ monkeyroom/engine.pl
@@ -14,5 +14,20 @@ action(state(atCenter, atCenter, onFloor, hasNot),jump,state(atCenter, atCenter,
 action(state(BoxPlaceX, BoxPlaceX, onFloor, hasNot),push(BoxPlaceX, BoxPlaceY),state(BoxPlaceY, BoxPlaceY, onFloor, hasNot)).	%push action
 action(state(PosX, BoxPlace, onFloor, hasNot),goto(PosX,PosY),state(PosY, BoxPlaceX, onFloor, hasNot)).				%goto action
 
+action(goto(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
+action(push(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
+action(jump).
+action(grab).
+
+coordX(atDoor).
+coordX(atWindow).
+coordX(atCenter).
+
 goal(state(_,_,_,has)).
-goal(OldState):-action(OldState,Option,NewState),goal(NewState).
+goal(OldState):-action(OldState,Action,NewState), action(Action), goal(NewState), (OldState\=NewState).
+
+/*
+ *	Below is line for faster debugging or program starting.
+ */
+check(X) :- (X=state(atWindow, atDoor, onFloor, hasNot)), goal(X).
+
