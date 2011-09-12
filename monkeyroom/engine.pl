f9314219e5d8a629f59b3044a2dacac5687bdab6
Uko <uko@unikernel.net>
2011-09-13 01:16:47 +0300

    Added some smart code
    Materialized idea, still need to find what's wrong.

--- monkeyroom/engine.pl
+++ monkeyroom/engine.pl
@@ -1,14 +1,18 @@
 /*
  *	atDoor, atWindows, atCenter
  *	onFloor, onBox
- *	state(BoxPlace, MonkeyH, MonkeyV, Has).
- *	goal(state(_,_,_,has)).
- *	mave(state(BoxPlace, X, onFloor, _),
- *	     goto(X,Y),
- *	     state(BoxPlace, Y, onFloor, _)).
+ *	has, hasNot
  *
  *	goto(X, Y).
  *	push(X, Y).
  *	jump
  *	grab
  */
+
+action(state(atCenter, atCenter, onBox, hasNot),grab,state(atCenter, atCenter, onBox, has)).					%jump action
+action(state(atCenter, atCenter, onFloor, hasNot),jump,state(atCenter, atCenter, onBox, hasNot)).				%jump action
+action(state(BoxPlaceX, BoxPlaceX, onFloor, hasNot),push(BoxPlaceX, BoxPlaceY),state(BoxPlaceY, BoxPlaceY, onFloor, hasNot)).	%push action
+action(state(PosX, BoxPlace, onFloor, hasNot),goto(PosX,PosY),state(PosY, BoxPlaceX, onFloor, hasNot)).				%goto action
+
+goal(state(_,_,_,has)).
+goal(OldState):-action(OldState,Option,NewState),goal(NewState).
