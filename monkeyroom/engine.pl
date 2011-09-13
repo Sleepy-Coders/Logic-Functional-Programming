830c5e04ceced83030ac6ba1abb8111bfc3d8a4e
anygizer <mcangel@unikernel.net>
2011-09-13 03:19:46 +0300

    Fixed typos. Almost works.
    Rolled back to previuos program model, fixed typos.
    Now monkey grabs the banana almost correctly. But as result of comparison with the most goal structure a new undefined variable appears (clearly seen in the trace list).
    
    Tip: DO NOT code at evening time, do it in the morning or... night :).

--- monkeyroom/engine.pl
+++ monkeyroom/engine.pl
@@ -10,24 +10,26 @@
  */
 
 action(state(atCenter, atCenter, onBox, hasNot),grab,state(atCenter, atCenter, onBox, has)).					%jump action
-action(state(atCenter, atCenter, onFloor, hasNot),jump,state(atCenter, atCenter, onBox, hasNot)).				%jump action
+action(state(BoxPlace, BoxPlace, onFloor, hasNot),jump,state(BoxPlace, BoxPlace, onBox, hasNot)).				%jump action
 action(state(BoxPlaceX, BoxPlaceX, onFloor, hasNot),push(BoxPlaceX, BoxPlaceY),state(BoxPlaceY, BoxPlaceY, onFloor, hasNot)).	%push action
-action(state(PosX, BoxPlace, onFloor, hasNot),goto(PosX,PosY),state(PosY, BoxPlaceX, onFloor, hasNot)).				%goto action
+action(state(BoxPlace, PosX, onFloor, hasNot),goto(PosX,PosY),state(BoxPlace, PosY, onFloor, hasNot)).				%goto action
 
-action(goto(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
+/*action(goto(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
 action(push(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
 action(jump).
 action(grab).
 
 coordX(atDoor).
 coordX(atWindow).
-coordX(atCenter).
+coordX(atCenter).*/
 
 goal(state(_,_,_,has)).
-goal(OldState):-action(OldState,Action,NewState), action(Action), goal(NewState), (OldState\=NewState).
+%goal(OldState):-action(OldState,Action,NewState), action(Action), goal(NewState), (OldState\=NewState).
+goal(OldState):-%(OldState = state(_,_,_,has));
+                action(OldState,_,NewState), goal(NewState).%, (OldState\=NewState).
 
 /*
  *	Below is line for faster debugging or program starting.
  */
-check(X) :- (X=state(atWindow, atDoor, onFloor, hasNot)), goal(X).
+check(_) :- goal(state(atWindow, atDoor, onFloor, hasNot)).
 
