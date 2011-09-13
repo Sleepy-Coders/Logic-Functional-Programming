24b754c4642942ed87a748bf57935e0a5feca429
Uko <uko@unikernel.net>
2011-09-13 14:03:15 +0300

    Now the solution of the monkey problem is working correctly and with output.
    Added an Action variable to the state change instead of anonymous one, and an output of it. The solution is outputted in reverse order.

--- monkeyroom/engine.pl
+++ monkeyroom/engine.pl
@@ -14,19 +14,21 @@ action(state(BoxPlace, BoxPlace, onFloor, hasNot),jump,state(BoxPlace, BoxPlace,
 action(state(BoxPlaceX, BoxPlaceX, onFloor, hasNot),push(BoxPlaceX, BoxPlaceY),state(BoxPlaceY, BoxPlaceY, onFloor, hasNot)).	%push action
 action(state(BoxPlace, PosX, onFloor, hasNot),goto(PosX,PosY),state(BoxPlace, PosY, onFloor, hasNot)).				%goto action
 
-/*action(goto(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
-action(push(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
-action(jump).
-action(grab).
-
-coordX(atDoor).
-coordX(atWindow).
-coordX(atCenter).*/
+/*
+*	action(goto(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
+*	action(push(From, Where)) :- coordX(From), coordX(Where), (From\=Where).
+*	action(jump).
+*	action(grab).
+*	
+*	coordX(atDoor).
+*	coordX(atWindow).
+*	coordX(atCenter).
+*/
 
 goal(state(_,_,_,has)).
 %goal(OldState):-action(OldState,Action,NewState), action(Action), goal(NewState), (OldState\=NewState).
 goal(OldState):-%(OldState = state(_,_,_,has));
-                action(OldState,_,NewState), goal(NewState).%, (OldState\=NewState).
+                action(OldState,Action,NewState), goal(NewState),writef("%w\t",[Action]).%, (OldState\=NewState).
 
 /*
  *	Below is line for faster debugging or program starting.
