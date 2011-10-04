b3bc4dc151cf5cc62d6ef30c71181b33634f1141
Uko <uko@unikernel.net>
2011-10-04 15:55:27 +0300

    Added database functions. Corrected atom text concatenation from hyphen to underscore.
    Hyphen works as an operator, so all hyphened words like: pale-lager were changed to pale_lager

--- dataBase/beerCountriesController.pl
+++ dataBase/beerCountriesController.pl
@@ -9,3 +9,13 @@ typeIs(Beer,Type):-country(_,_,Y),Y contains brewery(_,Z,_),Z contains beer(Beer
 
 drinksTheMostBeer(X):-country(X,Y1,_),not((country(_,Y2,_),Y2>Y1)).
 :-op(500,xf,drinksTheMostBeer).
+
+beersIn(0,[]).
+beersIn(N,[brewery(_,Beers,_)|Tail]):-beersIn(K,Tail),length(Beers,H),N is K+H.
+:-op(500,xfx,beersIn).
+
+breweriesOf(L,N):-country(N,_,L).
+:-op(500,xfx, breweriesOf).
+
+hasMostBeers(X):-country(X,_,Y1),not((country(_,_,Y2),Beers1 beersIn Y1, Beers2 beersIn Y2,Beers2>Beers1)).
+:-op(500,xf, hasMostBeers).
--- dataBase/db/beerCountries.pl
+++ dataBase/db/beerCountries.pl
@@ -3,18 +3,18 @@
 
 country('Czech Republic', 158, [
 	brewery('Pilsner Urquell', [beer('Pilsner Urquell', lager)],1842),
-	brewery('Budweiser Budvar', [beer('ORIGINAL', lager),beer('DARK',dark-lager)],1895),
-	brewery('Velkopopovicky Kozel',[beer('Pale',pale-lager),beer('Dark',dark-lager),beer('11° Medium', pale-lager),beer('Premium',pale-lager)],1874),
-	brewery('Staropramen',[beer('Premium',lager),beer('Dark', dark-lager),beer('Garnet',red-lager),beer('Light', pale-lager)],1869)
+	brewery('Budweiser Budvar', [beer('ORIGINAL', lager),beer('DARK',dark_lager)],1895),
+	brewery('Velkopopovicky Kozel',[beer('Pale',pale_lager),beer('Dark',dark_lager),beer('11° Medium', pale_lager),beer('Premium',pale_lager)],1874),
+	brewery('Staropramen',[beer('Premium',lager),beer('Dark', dark_lager),beer('Garnet',red_lager),beer('Light', pale_lager)],1869)
 ]).
 
 country('Germany', 110,[
-	brewery('Augustiner',[beer('Helles',light-lager),beer('Edelstoff',lager),beer('Dunkles',dark-lager),beer('Pils',pilsner),beer('Weissbier',wheat),beer('Oktoberfestbier',special),beer('Maximator',doppelbock),beer('Heller Bock',bock)],1328),
-	brewery('Pauleaner',[beer('Hefe-Weissbier Naturtrüb', wheat),beer('Hefe-Weissbier Dunkel',dark-wheat),beer('Hefe-Weissbier Alkoholfrei',non-alcohol),beer('Original Münchner Dunkel',dark-lager),beer('Original Münchner Urtyp',lager),beer('Premium Pils',pilsner),beer('Salvator',doppelbock)],1634),
-	brewery('Löwenbräu',[beer('Münchner Dunkel',dark-lager),beer('Triumphator',doppelbock),beer('Alkoholfrei',non-alcohol),beer('Pils',pilsner),beer('Dunkle Weisse',dark-wheat),beer('Weisse Hell',wheat),beer('Oktoberfestbier',special)],1383)
+	brewery('Augustiner',[beer('Helles',light_lager),beer('Edelstoff',lager),beer('Dunkles',dark_lager),beer('Pils',pilsner),beer('Weissbier',wheat),beer('Oktoberfestbier',special),beer('Maximator',doppelbock),beer('Heller Bock',bock)],1328),
+	brewery('Pauleaner',[beer('Hefe_Weissbier Naturtrüb', wheat),beer('Hefe_Weissbier Dunkel',dark_wheat),beer('Hefe_Weissbier Alkoholfrei',non_alcohol),beer('Original Münchner Dunkel',dark_lager),beer('Original Münchner Urtyp',lager),beer('Premium Pils',pilsner),beer('Salvator',doppelbock)],1634),
+	brewery('Löwenbräu',[beer('Münchner Dunkel',dark_lager),beer('Triumphator',doppelbock),beer('Alkoholfrei',non_alcohol),beer('Pils',pilsner),beer('Dunkle Weisse',dark_wheat),beer('Weisse Hell',wheat),beer('Oktoberfestbier',special)],1383)
 ]).
 
 country('Australia',104,[
 	brewery('Cascade Brewery',[beer('Premium Lager',lager),beer('Pale Ale',lager),beer('Blonde',lager),beer('Amber',ale),beer('Draught',lager),beer('Lager (Blue)',lager),beer('Bitter (Red)',lager),beer('Stout',stout)],1832),
-	brewery('Coopers',[beer('Sparkling Ale',ale),beer('Pale Ale',ale),beer('Best Extra Stout',stout),beer('Dark Ale',ale),beer('Mild Ale',ale),beer('Premium Lager',lager),beer('62 Pilsner',pilsner),beer('Extra Strong Vintage Ale',ale),beer('Birell', non-alcohol),beer('Dr. Tims Traditional Ale',ale)],1862)
+	brewery('Coopers',[beer('Sparkling Ale',ale),beer('Pale Ale',ale),beer('Best Extra Stout',stout),beer('Dark Ale',ale),beer('Mild Ale',ale),beer('Premium Lager',lager),beer('62 Pilsner',pilsner),beer('Extra Strong Vintage Ale',ale),beer('Birell', non_alcohol),beer('Dr. Tims Traditional Ale',ale)],1862)
 ]).
