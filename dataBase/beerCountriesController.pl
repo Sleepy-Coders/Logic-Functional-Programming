51cee235c64d5388acd6df8f7ab4a4ee18de463e
Uko <uko@unikernel.net>
2011-10-04 04:46:36 +0300

    Added some brain blowing functions to beerCountriesController

--- dataBase/beerCountriesController.pl
+++ dataBase/beerCountriesController.pl
@@ -1,1 +1,11 @@
 :-use_module(db/beerCountries).
+
+contains([X|_],X).
+contains([_|Tail],X):-contains(Tail,X).
+:-op(500,xfx,contains).
+
+typeIs(Beer,Type):-country(_,_,Y),Y contains brewery(_,Z,_),Z contains beer(Beer,Type).
+:-op(500,xfx,typeIs).
+
+drinksTheMostBeer(X):-country(X,Y1,_),not((country(_,Y2,_),Y2>Y1)).
+:-op(500,xf,drinksTheMostBeer).
