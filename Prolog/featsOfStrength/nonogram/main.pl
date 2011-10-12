e783f5eb971aa42975711ade951e2e27ee954826
Uko <uko@unikernel.net>
2011-10-12 12:07:40 +0300

    This solutions is acutely working.

--- Prolog/featsOfStrength/nonogram/main.pl
+++ Prolog/featsOfStrength/nonogram/main.pl
@@ -2,7 +2,8 @@
 
 generateCombos(RowCombos, ColCombos, Rows, Cols) :- length(Rows, VSize), length(Cols, HSize), generate(RowCombos, Rows, HSize), generate(ColCombos, Cols, VSize).
 
-generate(LineCombos, Lines, Size) :- generate_supp(1, LineCombos, Lines, Size).
+%### 1st gen ###
+/*generate(LineCombos, Lines, Size) :- generate_supp(1, LineCombos, Lines, Size).
 generate_supp(_, [], [], _).
 generate_supp(LineNumber, [line(LineNumber,Combos)|LineCombosTail], [Line|LinesTail], Size) :- findall(Combo, combinate(Line, Size, Combo), Combos), NextLineNumber is LineNumber + 1, generate_supp(NextLineNumber, LineCombosTail, LinesTail, Size).
 
@@ -10,3 +11,43 @@ combinate(Line, Size, Combo) :- sum(Sum, Line), length(Line, Length), CortegeLen
 combinate_supp([0], _, _, _, _, []).
 combinate_supp([Number], Start, _, Size, PreCombo, Combo) :- End is Size - Number + 1, inRange(First, Start, End), Last is First + Number - 1, range(EndCombo, First, Last), append(PreCombo, EndCombo, Combo).
 combinate_supp([Number|Tail], Start, CortegeLength, Size, PreCombo, Combo) :- End is Size - CortegeLength + 1, inRange(First, Start, End), Last is First + Number - 1, range(EndCombo, First, Last), NewCortegeLength is CortegeLength - Number - 1, NewStart is Last + 2, append(PreCombo, EndCombo, LowCombo), combinate_supp(Tail, NewStart, NewCortegeLength, Size, LowCombo, Combo).
+*/
+
+%### 2st gen ###
+generate([], [], _):-!.
+generate([Combo|CombosTail],[Line|LinesTail], Size) :- combine(Line, Size, Combo), generate(CombosTail, LinesTail, Size).
+
+% combine(Cortege, FrameSize, Combo).
+combine([0], FrameSize, Combo):-!,genList(Combo, FrameSize, 0).
+
+combine([X], FrameSize, Combo):-!,
+	FreeSpace is FrameSize - X,
+	inRange(Start, FreeSpace),
+	End is FreeSpace - Start,
+	genList(Part1, Start, 0),
+	genList(Part2, X, 1),
+	genList(Part3, End, 0),
+	append([Part1, Part2, Part3], Combo).
+combine([X|Cortage], FrameSize, Combo):-
+	sumContents(Cortage, CortageSum),
+	length(Cortage, CortageLength), 
+	FreeSpace is (FrameSize - CortageSum - CortageLength - X),
+	inRange(Start, FreeSpace),
+	genList(Part1,Start,0),
+	genList(Part2,X,1),
+	CortageFrame is (FrameSize - Start - X - 1),
+	combine(Cortage, CortageFrame, Part4),
+	append([Part1,Part2,[0],Part4],Combo).
+	
+nonogram(Rows,Cols):-length(Rows, VSize), length(Cols, HSize), generate(RowCombo, Rows, HSize), generate(ColCombo, Cols, VSize), rotate(RowCombo,ColCombo), writeNono(RowCombo).
+allCombos(Rows,Cols,X,Y):-length(Rows, VSize), length(Cols, HSize), findall(RowCombo,generate(RowCombo, Rows, HSize),X), findall(ColCombo,generate(ColCombo, Cols, VSize),Y).
+genRot(RowCombo, Cols, VSize):-generate(X, Cols, VSize),rotate(X,RowCombo).
+allCombosT(Rows,Cols,X,Y):-length(Rows, VSize), length(Cols, HSize), findall(RowCombo,genRot(RowCombo, Rows, HSize),X), findall(ColCombo,generate(ColCombo, Cols, VSize),Y).
+
+writeNono([]).
+writeNono([X|Tail]):-writeLine(X),write('\n'),writeNono(Tail).
+
+writeLine([]).
+writeLine([X|Tail]):-(X=:=1,write('#');X=:=0,write(' ')),writeLine(Tail).
+
+%sth(Rows,Cols):-length(Rows, VSize), length(Cols, HSize), generate(RowCombos, Rows, HSize), generate(_, Cols, VSize), matrix(ZZZ,RowCombos), transpose(ZZZ,XXX),write(XXX).
--- Prolog/featsOfStrength/nonogram/support.pl
+++ Prolog/featsOfStrength/nonogram/support.pl
@@ -1,17 +1,55 @@
 :-module(support,[
-	sum/2,
+	sumContents/2,
+	inRange/2,
 	inRange/3,
-	range/3
+	append/2,
+	range/3,
+	genList/3,
+	reverse/2,
+	rotate/2,
+	rotate/3
 	]).
 
-sum(0, []).
-sum(Sum,[Head|Tail]):-sum(K,Tail),Sum is K+Head.
+reverse([],[]).
+reverse([X|R],Reversed) :- reverse(R, RevRest), append(RevRest,[X],Reversed).
 
-% inRange(I, Low, Hi) I is between Low and Hi
+sumContents([], 0):-!.
+sumContents([Head|Tail],Sum):-sumContents(Tail,K),Sum is K+Head.
+
+% inRange(I, Low, Hi) X is between Low and Hi
+inRange(X, High) :- inRange(X, 0, High).
 inRange(Low, Low, High) :- Low =< High.
-inRange(I, Low, High) :- Low < High, LessLow is Low + 1, inRange(I, LessLow, High).
+inRange(X, Low, High) :- Low < High, LessLow is Low + 1, inRange(X, LessLow, High).
+
+append([],[]):-!.
+append([X|Tail],Z) :- append(X,Y,Z), append(Tail,Y).
 
 range([N], N, N).
 range([Low|Tail], Low, High) :- Low < High, LesLow is Low + 1, range(Tail, LesLow, High).
 
+genList([],0,_).
+genList(L,Size,Fill) :- length(L,Size),containsOnly(L,Fill).
 
+containsOnly([X],X).
+containsOnly([X|Tail],X) :- containsOnly(Tail,X).
+
+contains([X|_],X).
+contains([_|Tail],X):-contains(Tail,X).
+
+matrix(A,Rows):-matrix_p(1,A,Rows),length(Rows,Length),length(A, Length).
+matrix_p(0,[],_).
+matrix_p(N,[X],A):-contains(A,line(N,Y)),contains(Y,X).
+matrix_p(N,[X|Tail],A):-contains(A,line(N,Y)),contains(Y,X),N1 is N+1,matrix_p(N1,Tail,A).
+
+rotate([], []).
+rotate([F|Fs], Ts) :-
+    rotate(F, [F|Fs], Ts).
+
+rotate([], _, []).
+rotate([_|Rs], Ms, [Ts|Tss]) :-
+        lists_firsts_rests(Ms, Ts, Ms1),
+        rotate(Rs, Ms1, Tss).
+
+lists_firsts_rests([], [], []).
+lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
+        lists_firsts_rests(Rest, Fs, Oss).
