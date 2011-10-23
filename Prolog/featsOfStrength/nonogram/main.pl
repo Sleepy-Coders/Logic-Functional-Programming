28ce36f421edd37d28c64b81e32a24b6b96bce1d
Uko <uko@unikernel.net>
2011-10-23 18:26:27 +0300

    Remastered program structure.
    Moved defined monograms to a new module called: data.pl
    Deleted some not really needed predicates, and defined two for an easy solving purpose.

--- Prolog/featsOfStrength/nonogram/main.pl
+++ Prolog/featsOfStrength/nonogram/main.pl
@@ -1,25 +1,32 @@
 :-use_module(support).
-
-generateCombos(RowCombos, ColCombos, Rows, Cols) :- 
-		length(Rows, VSize),
-		length(Cols, HSize),
-		generate(RowCombos, Rows, HSize),
-		generate(ColCombos, Cols, VSize).
-
+:-use_module(data).
+		
+%%		
+%	Generates all possible combos for passed lines conditions, and length
+%%
 generate(LineCombos, Lines, Size) :- generate_supp(1, LineCombos, Lines, Size).
 
+%%
+%	Supportive predicate for generate/3 to include line number
+%%
 generate_supp(_, [], [], _).
 generate_supp(LineNumber, [line(LineNumber,Combos)|LineCombosTail], [Line|LinesTail], Size) :- 
 		findall(Combo, combinate(Line, Size, Combo), Combos),
 		NextLineNumber is LineNumber + 1,
 		generate_supp(NextLineNumber, LineCombosTail, LinesTail, Size).
 
+%%
+%	Returns possible line for passed conditions and size
+%%
 combinate(Line, Size, Combo) :- 
 		sum(Sum, Line),
 		length(Line, Length),
 		CortegeLength is Sum + Length - 1,
 		combinate_supp(Line, 1, CortegeLength, Size, [], Combo).
 
+%%
+%	Supportive predicate for combinate/3
+%%
 combinate_supp([0], _, _, _, _, []).
 combinate_supp([Number], Start, _, Size, PreCombo, Combo) :- 
 		End is Size - Number + 1,
@@ -60,23 +67,15 @@ linesMatch([line(RowNumber, [RowCombHead|RCTail])|RowsTail], Cols, MatchedLines)
 		combMatch(RowNumber, RowCombHead, Cols, MatchedRefinedCols), MatchedRefinedCols \= [], replaceLines(Cols, MatchedRefinedCols, ColsWithMatchedRefinedCols), MatchedLines = [/*line(RowNumber, [*/RowCombHead/*])*/|MLTail], linesMatch(RowsTail, ColsWithMatchedRefinedCols, MLTail);
 		linesMatch([line(RowNumber, RCTail)|RowsTail], Cols, MatchedLines).
 
-
-
-
-
 %%
-%	Tests data
+%	Nonogram printing.
 %%
-testData(windjammer,										% testName
-	rows([[1], [3], [1, 2], [1, 3], [2, 4], [7], [1], [10], [8]]),				% rows
-	cols([[1], [2, 2], [1, 2, 2], [1, 1, 2], [9], [5, 2], [3, 2], [2, 2], [1, 2], [1]])).	% columns
-testData(smile,
-	rows([[1, 1], [1, 1], [3]]),
-	cols([[1], [1, 1], [1], [1, 1], [1]])).
+printlist([]).
+printlist([H|T]) :- number(H), swritef(S, '~` tX~%w|', [H]), format(S, []), printlist(T).
 
-testCombos(TestName, RowCombos, ColCombos) :-
-		testData(TestName, rows(Rows), cols(Cols)),
-		generateCombos(RowCombos, ColCombos, Rows, Cols).
+printNonogram([]).
+printNonogram([H|T]):- printlist(H), nl, printNonogram(T).
+
 
 execTestAndWriteRess(TestName, M):-
 		nl,nl,write(TestName),write(':'),
@@ -87,23 +86,13 @@ execTestAndWriteRess(TestName, M):-
 		linesMatch(R, C, M),
 		nl, write('Result:\t'), write(M).
 
-execTest(TestName,M):-testData(TestName, rows(R), cols(C)),generateCombos(RC, CC, R, C),linesMatch(RC, CC, M).
-
-
-/**
- *	Nonogram printing.
- */
-printlist([]).
-printlist([H|T]) :- number(H), swritef(S, '~` tX~%w|', [H]), format(S, []), printlist(T).
-
-printNonogram([]).
-printNonogram([H|T]):- printlist(H), nl, printNonogram(T).
-
-:-nl,write('Tests start. Please wait for "Tests done." message.'),nl,nl.
-:-execTestAndWriteRess(smile, N), nl, printNonogram(N).
-:-execTestAndWriteRess(windjammer, N), nl, printNonogram(N).
-:-nl,write('Tests done.').
-%:-nl,write('Tests profiling start. Please wait for "Tests profiling done" message.').
-%:-nl,write('smile:'),nl,	profile(execTest(smile,_)).
-%:-nl,write('windjammer:'),nl,	profile(execTest(windjammer,_)).
-%:-nl,write('Tests profiling done.'),nl,nl.
+%%
+%	Solving nonograns.
+%%
+solveNonogram(Rows,Cols,Solution):-
+		length(Rows, VSize),
+		length(Cols, HSize),
+		generate(RowCombos, Rows, HSize),
+		generate(ColCombos, Cols, VSize),
+		linesMatch(RowCombos, ColCombos, Solution).
+solveNonogram(Nonogram):-nonogram(Nonogram,Rows,Cols),solveNonogram(Rows,Cols,Solution),printNonogram(Solution).
