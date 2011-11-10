89af1545670e46eb186da92e05d038ffbcd3a08d
Uko <uko@unikernel.net>
2011-11-11 01:20:19 +0200

    HURA! Finally implemented intersection and union. Improved writ's.
    Now r-write and i-write can use an optional arg. to determine amount of digits after dot.
    Intersection works recursively, union - iteratively.

--- Lisp/N17.lisp
+++ Lisp/N17.lisp
@@ -20,15 +20,16 @@
 ;END; Data loading
 
 ;START Data output
-(defun r-write (lst)
+(defun r-write (lst &optional (accuracy 1))
+(let ((last (format nil "~~d/~~~d,~df" (+ accuracy 2) accuracy)) (mid (format nil "~~d/~~~d,~df, ~~a" (+ accuracy 2) accuracy)))
   (if (cdr lst)
-      (format nil "~d/~3,1f, ~a" (caar lst) (cdar lst) (r-write (cdr lst))) 
-      (format nil "~d/~3,1f" (caar lst) (cdar lst) )))
+      (format nil mid (caar lst) (cdar lst) (r-write (cdr lst) accuracy)) 
+      (format nil last (caar lst) (cdar lst) ))))
 
-(defun i-write (lst)
-  (let ((str (format nil "~d/~3,1f" (caar lst) (cdar lst))))
+(defun i-write (lst &optional (accuracy 1))
+  (let ((str (format nil (format nil "~~d/~~~d,~df" (+ accuracy 2) accuracy) (caar lst) (cdar lst))) (mid (format nil "~~a, ~~d/~~~d,~df" (+ accuracy 2) accuracy)) )
     (dolist (i (cdr lst))
-      (setq str (format nil "~a, ~d/~3,1f" str (car i) (cdr i))))
+      (setq str (format nil mid str (car i) (cdr i))))
     str))
 
 ;;use:
@@ -59,20 +60,62 @@
   (dolist (i lst)
     (if (eq (car i) item) (return-from i-probability (cdr i)) nil)))
 
-#|
-(defun dintersection (lst1 lst2)
+
+(defun dintersection (lst1 lst2 &key (content #'i-contains) (probability #'r-probability))
   (cond
     ((eq lst1 nil) nil)
-    ((probability lst2 (caar lst1)) (cons
-				     (cons
-				      (caar lst1)
-				      (-
-				       (+
-					(cdar lst1)
-					(probability lst2 (caar lst1)))
-				       (*
-					(cdar lst1)
-					(probability lst2 (caar lst1)))))
-				     (dintersection (cdr lst1) lst2)))
-    (t (dintersection (cdr lst1) lst2))))
+    ((funcall content lst2 (caar lst1))
+     (cons
+      (cons
+       (caar lst1)
+       (*
+	(cdar lst1)
+	(funcall probability lst2 (caar lst1))))
+       (dintersection (cdr lst1) lst2 :content content :probability probability)))
+     (t (dintersection (cdr lst1) lst2 :content content :probability probability))))
+
+(defun dunity (lst1 lst2 &key (content #'i-contains) (probability #'r-probability))
+  (let ((result nil))
+    (if lst1
+	(dolist (i lst1)
+	  (if (funcall content lst2 (car i))
+	      (setq result
+	       (cons 
+		(cons
+		 (car i)
+		 (-
+		  (+
+		   (cdr i)
+		   (funcall probability lst2 (car i)))
+		  (*
+		   (cdr i)
+		   (funcall probability lst2 (car i)))))
+		result))
+	      (setq result (cons i result)))))
+    (if lst2
+	(dolist (i lst2)
+	  (if (funcall content lst1 (car i))
+	      (setq result (cons i result)))))
+    result))
+
+
+#| good idea bad knowladge: list isn't modified
+(defun dunity (lst1 lst2 &key (content #'i-contains) (probability #'r-probability))
+  (let ((result lst1) (i 0) (length (list-length lst2)))
+    (if lst2
+	(loop while (< i length) do
+	  (if (funcall content result (car (nth i lst2)))
+	      (setf (nth i lst2)
+		    (cons
+		     (car (nth i lst2))
+		     (-
+		      (+
+		       (cdr (nth i lst2))
+		       (funcall probability lst2 (car (nth i lst2))))
+		      (*
+		       (cdr (nth i lst2))
+		       (funcall probability lst2 (car (nth i lst2)))))))
+	      (setq result (cons (nth i lst2) result) ))
+	     (setq i (+ i 1))))
+    result))
 |#
