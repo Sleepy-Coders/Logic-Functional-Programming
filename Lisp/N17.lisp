b99f8f997856433eed565dc6d590f994251e0a68
Uko <uko@unikernel.net>
2011-11-10 17:04:55 +0200

    Improved N17, added one more datafile for N17
    Remastered input/write with optional arguments and defaults. Added some set-centric functions like intersection…

--- Lisp/N17.lisp
+++ Lisp/N17.lisp
@@ -1,35 +1,81 @@
-(defun load-recursively (in)
+;START; Data loading
+(defun r-load (in)
   (if (listen in)
-      (cons (cons (read in nil) (read in nil)) (load-iteratively in)) 
+      (cons (cons (read in nil) (read in nil)) (r-load in)) 
       nil))
 
-(defun load-iteratively (in)
+(defun i-load (in)
   (let ((vect `()))
     (loop while (listen in) do
 	 (setq vect (append vect (list (cons (read in) (read in))))))
     vect))
 
 ;;use:
-;;(data "filename" #'load-iteratively)
-;;(data "filename" #'load-recursively)
-(defun data (what how)
+;(list-from-file "filename")
+;(list-from-file "filename" #'r-load)
+;(list-from-file "filename" #'i-load)
+(defun list-from-file (what &optional (how #'r-load))
   (with-open-file (in what)
     (funcall how in)))
+;END; Data loading
 
-(defun write-recursively (lst)
+;START Data output
+(defun r-write (lst)
   (if (cdr lst)
-      (format nil "~d/~3,1f, ~a" (caar lst) (cdar lst) (write-recursively (cdr lst))) 
+      (format nil "~d/~3,1f, ~a" (caar lst) (cdar lst) (r-write (cdr lst))) 
       (format nil "~d/~3,1f" (caar lst) (cdar lst) )))
 
-(defun write-iteratively (lst)
+(defun i-write (lst)
   (let ((str (format nil "~d/~3,1f" (caar lst) (cdar lst))))
     (dolist (i (cdr lst))
       (setq str (format nil "~a, ~d/~3,1f" str (car i) (cdr i))))
     str))
 
 ;;use:
-;;(out-set <list> #'write-iteratively)
-;;(out-set <list> #'write-recursively)
-(defun out-set (lst how)
-  (format nil "A={~a}" (funcall how lst)))
-  
+;(write-list <list>)
+;(write-list <list> #'r-write)
+;(write-list <list> #'i-write)
+(defun write-list (lst &optional (how #'r-write))
+  (format nil "{~a}" (funcall how lst)))
+;END; Data output
+
+(defun contains (lst item)
+  (if lst
+      (or (= (caar lst) item) (contains (cdr lst) item))
+      nil))
+
+(defun probability (lst item)
+  (if lst
+      (if (= (caar lst) item)
+	  (cdar lst)
+	  (probability (cdr lst) item))
+      nil))
+
+#|(defun probability-i (lst item)
+  (if lst
+      (let ((i 0))
+	(loop while (and (< i (list-length lst)) (eq item (car (nth i lst))))
+			 do (setq i (+ i 1)))
+	(cdr (nth i lst))|#
+
+#|(defun probability-i (lst item)
+  (if lst
+      (dolist (i lst)
+	(if (= item )|#
+
+(defun dintersection (lst1 lst2)
+  (cond
+    ((eq lst1 nil) nil)
+    ((probability lst2 (caar lst1)) (cons
+				     (cons
+				      (caar lst1)
+				      (-
+				       (+
+					(cdar lst1)
+					(probability lst2 (caar lst1)))
+				       (*
+					(cdar lst1)
+					(probability lst2 (caar lst1)))))
+				     (dintersection (cdr lst1) lst2)))
+    (t (dintersection (cdr lst1) lst2))))
+
