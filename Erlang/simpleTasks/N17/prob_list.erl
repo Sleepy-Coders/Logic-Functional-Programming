89b806f56f29970250962fdb4aca4aa01ebcb910
Uko <uko@unikernel.net>
2011-11-13 02:35:56 +0200

    Added to Erlang: `union` and `intersection` functions, one more datafile, unit-tests
    Testing of the private functions is not working!!!

--- Erlang/simpleTasks/N17/prob_list.erl
+++ Erlang/simpleTasks/N17/prob_list.erl
@@ -1,12 +1,34 @@
 -module(prob_list).
--export([contains/2,probability/2]).
+-export([intersection/2,union/2]).
+
+intersection([], _List) -> [];
+intersection(_List, []) -> []; 
+intersection([{Item, Probability}|Rest1], List2) -> Contains = contains(List2, Item),
+    if
+	Contains -> [{Item,Probability*probability(List2, Item)}|intersection(Rest1, List2)];
+	true     -> intersection(Rest1, List2)
+    end.
+
+union([], List) -> List;
+union(List, []) -> List;
+union([{Item, Probability}|Rest1], List2) -> Contains = contains(List2, Item),
+    if
+	Contains -> [{Item,Probability+probability(List2, Item)-Probability*probability(List2, Item)}|union(Rest1, lesslist(List2, Item))];
+	true     -> [{Item,Probability}|union(Rest1, List2)]
+    end.
+
+%%   SSS U  U PPP  PPP   OO  RRR  TTTTT
+%%  S    U  U P  P P  P O  O R  R   T
+%%   SS  U  U PPP  PPP  O  O RRR    T
+%%     S U  U P    P    O  O R R    T
+%%  SSS   UU  P    P     OO  R  R   T
 
 contains([], _Item) -> 
     false;
 contains([{First,_Probability}|Rest], Item) ->
     if
 	First == Item -> true;
-	true -> contains(Rest, Item)
+	true          -> contains(Rest, Item)
     end.
 				 
 probability([], _Item) ->
@@ -14,6 +36,12 @@ probability([], _Item) ->
 probability([{First,Probability}|Rest], Item) ->
     if
 	First == Item -> Probability;
-	true -> probability(Rest, Item)
+	true          -> probability(Rest, Item)
     end.
 
+lesslist([], _Item) -> [];
+lesslist([{First,Probability}|Rest], Item) ->
+    if
+        First == Item -> Rest;
+	true -> [{First,Probability}|lesslist(Rest,Item)]
+    end.
--- Erlang/simpleTasks/N17/prob_list_tests.erl
+++ Erlang/simpleTasks/N17/prob_list_tests.erl
@@ -2,14 +2,15 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -define(TEST_LIST,[{3,0.2},{4,0.6},{5,1.0},{6,0.5}]).
+-define(TEST_LIST1,[{2,0.9},{3,0.6},{6,0.1},{8,0.5}]).
 -define(TEST_UNO_LIST,[{2,0.5}]).
 
 contains_test() -> ?assertNot(prob_list:contains([],3)),
-		    ?assert(prob_list:contains(?TEST_UNO_LIST,2)),
-		    ?assertNot(prob_list:contains(?TEST_UNO_LIST,3)),
-		    ?assert(prob_list:contains(?TEST_LIST,3)),
-		    ?assert(prob_list:contains(?TEST_LIST,5)),
-		    ?assertNot(prob_list:contains(?TEST_LIST,7)).
+		   ?assert(prob_list:contains(?TEST_UNO_LIST,2)),
+		   ?assertNot(prob_list:contains(?TEST_UNO_LIST,3)),
+		   ?assert(prob_list:contains(?TEST_LIST,3)),
+		   ?assert(prob_list:contains(?TEST_LIST,5)),
+		   ?assertNot(prob_list:contains(?TEST_LIST,7)).
 
 probability_test() -> ?assertNot(prob_list:probability([],3)),
 		      ?assertEqual(0.5,prob_list:probability(?TEST_UNO_LIST,2)),
@@ -17,3 +18,18 @@ probability_test() -> ?assertNot(prob_list:probability([],3)),
 		      ?assertEqual(0.2,prob_list:probability(?TEST_LIST,3)),
 		      ?assertEqual(1.0,prob_list:probability(?TEST_LIST,5)),
 		      ?assertNot(prob_list:probability(?TEST_LIST,7)).
+
+lesslist_test() -> ?assertEqual([],prob_list:lesslist([],3)).
+
+intersection_test() -> ?assertEqual([],prob_list:intersection([],[])),
+		       ?assertEqual([],prob_list:intersection([],?TEST_LIST)),
+		       ?assertEqual([],prob_list:intersection(?TEST_LIST,[])),
+		       ?assertEqual([],prob_list:intersection(?TEST_LIST, ?TEST_UNO_LIST)),
+		       ?assertEqual([{2,0.45}],prob_list:intersection(?TEST_UNO_LIST, ?TEST_LIST1)),
+		       ?assertEqual([{3,0.12},{6,0.05}],prob_list:intersection(?TEST_LIST, ?TEST_LIST1)).
+
+union_test() -> ?assertEqual([],prob_list:union([],[])),
+		?assertEqual(?TEST_LIST,prob_list:union(?TEST_LIST,[])),
+		?assertEqual(?TEST_LIST,prob_list:union([],?TEST_LIST)),
+		?assertEqual([{2,0.5},{3,0.2},{4,0.6},{5,1.0},{6,0.5}],prob_list:union(?TEST_UNO_LIST, ?TEST_LIST)),
+		?assertEqual([{2,0.95},{3,0.6},{6,0.1},{8,0.5}],prob_list:union(?TEST_UNO_LIST, ?TEST_LIST1)).
